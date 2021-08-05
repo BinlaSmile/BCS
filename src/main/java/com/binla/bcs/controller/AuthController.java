3d00a015f68b20255c18c7d5426cf5ebc8879610
Binla <495083320@qq.com>
2021-08-05 18:02:41 +0800

    补充授权接口

--- src/main/java/com/binla/bcs/controller/AuthController.java
+++ src/main/java/com/binla/bcs/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.binla.bcs.core.annotation.ResponseResult;
 import com.binla.bcs.domain.Response;
 import com.binla.bcs.domain.constants.SecurityConstant;
 import com.binla.bcs.model.auth.AuthResultModel;
+import com.binla.bcs.model.auth.CurrentUserModel;
 import com.binla.bcs.model.auth.LoginModel;
 import com.binla.bcs.domain.enums.CodeMsg;
 import com.binla.bcs.service.IAuthService;
@@ -20,6 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @Api(tags = "Auth")
 @RequestMapping("/api/auth")
+@ResponseResult
 public class AuthController {
 
     @Autowired
@@ -27,7 +29,7 @@ public class AuthController {
 
     @PostMapping
     @ApiOperation(value = "获取授权")
-    @ResponseResult
+
     public AuthResultModel getAuthToken(@RequestBody LoginModel request, HttpServletResponse response) {
         String token = authService.authLogin(request.getUserCode(),request.getPassword());
         response.setHeader(SecurityConstant.AUTH_HEADER, token);
@@ -35,9 +37,14 @@ public class AuthController {
         return new AuthResultModel(request.getUserCode(),token);
     }
 
+    @GetMapping("/currentUser")
+    @ApiOperation(value = "获取当前登录用户信息")
+    public CurrentUserModel getCurrentUser(){
+        return authService.getCurrentUser();
+    }
+
     @DeleteMapping("/logout")
     @ApiOperation(value = "当前用户退出登录")
-    @ResponseResult
     public Response authLogout(){
         authService.logout();
         return Response.success();
